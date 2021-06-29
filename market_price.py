272c4188cbf1e3b803639bf83489b4c3f236d380
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2021-06-29 17:37:52 -0500

    Mejora en los datos de las criptos, se filtran aproximadamente 50 criptos

--- market_price.py
+++ market_price.py
@@ -1,155 +1,103 @@
 import json
-import requests
-import time
 from firebase import firebase
-
-
-def place(str_price, cripto):
-    global mapcrypto
-    numeros = []
-
-    for x in range(0, 10):
-        x_str = str(x)
-        x_price = str_price.find(x_str)
-
-        if x_price != -1 and x_price > 25:
-            numeros.append(x_price)
-
-        elif 0 < x_price < 25:  # Excepción para KP3RBNB
-            f_price = str_price[32:41]  # Corregir
-
-    s_price = min(numeros) + str_price[min(numeros):].find("'")
-    f_price = str_price[min(numeros):s_price]
-
-    mapcrypto = {'Symbol': cripto, 'Price': float(f_price)}
-
-    # print("")
-    # print(f_price)
-    # print(mapcrypto)
-
-
-mapcrypto = {}
+import requests
 
 
 class Price:
     def __init__(self):
         pass
+final_coin = []
 
-    count = 0
-    count_1 = 1
-    n_coins = 1528
+firebase = firebase.FirebaseApplication('https://python-cryptoapp-default-rtdb.firebaseio.com/')
+all_binance_coins = requests.get("https://api.binance.com/api/v3/ticker/24hr")
+pendiente = json.loads(all_binance_coins.text)
 
-    firebase = firebase.FirebaseApplication('https://python-cryptoapp-default-rtdb.firebaseio.com/')
-    response = requests.get("https://api.binance.com/api/v3/ticker/price")
-    pendiente = json.loads(response.text)
-    prices = json.dumps(pendiente)
+def filter_data(pendiente, buscador):
+    def iterator_func(x):
+        for v in x.values():
+            if buscador in v:
+                return True
+        return False
+    return filter(iterator_func, pendiente)
 
-    with open("D:\Python Code\python_stock\price.json", "w", encoding="utf-8") as f:
-        f.write(prices)
+for x in pendiente:
+    x.pop('count')
+    x.pop('firstId')
+    x.pop('lastId')
+    x.pop('closeTime')
+    x.pop('openTime')
 
-    with open("D:\Python Code\python_stock\price.txt", "w+", encoding="utf-8") as f:
-        list_criptomoneda = []
+final_coin.extend(list(filter_data(pendiente, 'BTCUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'ETHUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'BNBUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'ADAUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'DOGEUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'XRPUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'DOTUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'UNIUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'BHCUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'LTCUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'SOLUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'LINKUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'MATICUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'THETAUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'WBTCUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'ICPUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'XLMUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'ETCUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'VETUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'TRXUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'FILUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'XRMUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'EOSUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'SHIBUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'AAVEUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'BSVUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'CROUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'ALGOUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'KLAYUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'CAKEUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'AMPUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'FTTUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'NEOUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'XTZUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'LUNAUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'ATOMUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'MIOTAUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'LEOUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'TFUELUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'KSMUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'AVAXUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'HTUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'BTTUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'DCRUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'GRTUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'WAVESUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'CHZUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'ZCASHUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'HOTUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'SUSHIUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'ENJUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'MDXUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'DGBUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'OKBUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'SCUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'RVNUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'BAKEUSDT')))
+final_coin.extend(list(filter_data(pendiente, '1INCHUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'IOSTUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'WINUSDT')))
+final_coin.extend(list(filter_data(pendiente, 'WAXUSDT')))
 
-        for price in pendiente:
-            count = count + 1
-            str_price = str(price)
-            f.write(str_price)
-            f.write("\n")
+with open("D:\python_stock\price.txt", "w+", encoding="utf-8") as f:
+    for x in final_coin:
+        f.write(str(x))
+        f.write("\n")
 
-            if str_price[17] == "'":
-                cripto = str_price[12:17]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                #print('5: ' + cripto)
+resultado = firebase.post('/CRIPTOMONEDAS', final_coin)
 
-            elif str_price[18] == "'":
-                cripto = str_price[12:18]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                # print('5: ' + cripto)
-
-            elif str_price[19] == "'":
-                cripto = str_price[12:19]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                # print('5: ' + cripto)
-
-            elif str_price[20] == "'":
-                cripto = str_price[12:20]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                # print('5: ' + cripto)
-
-            elif str_price[21] == "'":
-                cripto = str_price[12:21]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                # print('5: ' + cripto)
-
-            elif str_price[22] == "'":
-                cripto = str_price[12:22]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                # print('5: ' + cripto)
-
-            elif str_price[23] == "'":
-                cripto = str_price[12:23]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                # print('5: ' + cripto)
-
-            elif str_price[24] == "'":
-                cripto = str_price[12:24]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                # print('5: ' + cripto)
-
-            elif str_price[25] == "'":
-                cripto = str_price[12:25]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                # print('5: ' + cripto)
-
-            elif str_price[21] == "'":  # No existe cripto con más de 13 letras
-                cripto = str_price[12:26]
-                place(str_price, cripto)
-                list_criptomoneda.append(mapcrypto)
-                # print('5: ' + cripto)
-
-
-
-            #if str_price[12:20] == 'CAKEUSDT':
-                #CAKEUSDT = str_price[12:20]
-                #price_CAKEUSDT = str_price[33:43]
-                #_price_CAKEUSDT = float(str_price[33:43])
-
-                # mapCAKEUSDT = {CAKEUSDT: _price_CAKEUSDT}
-
-                # print("\n" + CAKEUSDT)
-                # print('Precio: ' + price_CAKEUSDT)
-                # print(mapCAKEUSDT)
-
-            #elif str_price[12:20] == 'CAKEBUSD':
-                #CAKEBUSD = str_price[12:20]
-                #price_CAKEBUSD = str_price[33:43]
-                #_price_CAKEBUSD = float(price_CAKEBUSD)
-                #mapCAKEBUSD = {CAKEBUSD: _price_CAKEBUSD}
-
-                # print("\n" + CAKEBUSD)
-                # print('Precio: ' + price_CAKEBUSD)
-
-            if count == 1528:
-
-                # mapcriptomoneda = mapCAKEUSDT, mapCAKEBUSD
-                # mapcriptomoneda.update(mapCAKEUSDT)
-                # mapcriptomoneda.update(mapCAKEBUSD)
-                print('\nAPP Actualizada\n')
-                # print(mapcriptomoneda)
-
-            elif count > 1528:
-                print('Actualizar APP, Nueva Criptomoneda Añadida')
-
-    #print(mapcriptomoneda)
-    #print(list_criptomoneda)
-    resultado = firebase.post('/CRIPTOMONEDAS', list_criptomoneda)
+if resultado:
+    print('Datos Cargados')
+else:
+    print('Datos no cargados')
 
